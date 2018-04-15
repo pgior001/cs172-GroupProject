be6f39b08e81e085ca7f955b08c43a9221be52cb
pgior001 <pgior001@ucr.edu>
2018-04-14 17:02:51 -0700

    basic crawling functionality
    still need to add robots.txt.check, handle duplicate urls, save the html files, and much more this is just a basic crawler that will move from page to page as it finds links

--- webCrawler/src/com/web/crawler/CrawlerThread.java
+++ webCrawler/src/com/web/crawler/CrawlerThread.java
@@ -1,44 +1,67 @@
 package com.web.crawler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class CrawlerThread extends Thread {
 	int threadNumber;
-	private static Queue<String> urlsToCrawl = new LinkedList<String>();
+	private static Queue<String> urlsToCrawl = new LinkedList<String>(Arrays.asList("http://www.ucr.edu/", "https://www.american.edu/", "https://www.sdsu.edu/" 
+			, "http://www.rccd.edu", "https://ucsd.edu/", "https://www.berkeley.edu/", "https://uci.edu/", "http://www.ucla.edu/", "https://www.csusm.edu/", 
+			"http://www.cpp.edu/", "http://www.redlands.edu/", "https://calbaptist.edu/", "https://www.apu.edu/"));
 	
 	public CrawlerThread(int number) {
 		threadNumber = number;
 	}
 	
 	public void run() {
-	        System.out.println("thread started: " + threadNumber);
+	        this.crawl();
+    }
+	 private void crawl() {
+		 System.out.println("thread started: " + threadNumber);
 	        //use code like this to make sure only one thread manipulates urls at a time.
-	        synchronized(urlsToCrawl) {
-	        	if(!urlsToCrawl.isEmpty()) {
-	        		String url = urlsToCrawl.remove();
+	        String url = getNextUrl();
+	        while(url != null) {
+	        	ArrayList<String> found = this.retriveDocumentLinks(url);
+	        	for(int i = 0; i < found.size(); ++i) {
+	        		urlsToCrawl.add(found.get(i));
 	        	}
+	        	url = getNextUrl();
 	        }
     }
-	 private static void crawl() {
-	    	
-    }
+	 
+	 private String getNextUrl() {
+		 synchronized(urlsToCrawl) {
+	        	if(!urlsToCrawl.isEmpty()) {
+	        		return urlsToCrawl.remove();
+	        	} else {
+	        		return null;
+	        	}
+	        }
+	 }
 	    
-    private void retriveDocumentLinks(String URL) {
+    private ArrayList<String> retriveDocumentLinks(String URL) {
     	Document doc;
+		ArrayList<String> linkUrls = new ArrayList<String>();
 		try {
 			doc = Jsoup.connect(URL).get();
-			Element link = doc.select("a").first();
-	    	String relHref = link.attr("href"); // == "/"
-	    	String absHref = link.attr("abs:href"); // "http://jsoup.org/"
+			Elements links = doc.select("a[href]");
+			for(int i = 0; i < links.size(); ++i) {
+				linkUrls.add(links.get(i).attr("abs:href"));
+			}
+//	    	String relHref = link.attr("href"); // == "/"
+//	    	String absHref = link.attr("abs:href"); // "http://jsoup.org/"
+	    	System.out.println(threadNumber + ": " + linkUrls);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	return linkUrls;
     }
 }
